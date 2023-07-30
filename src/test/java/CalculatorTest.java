b5810089ed0f34808a75458358e15b4ea5f03c77
Andrii Protas <andreyprotas5@gmail.com>
2023-07-30 13:40:50 +0200

    Create tests for Calculator class

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+
+    @BeforeEach
+    public void startTime() {
+        System.out.println("Test started at: " + Instant.now());
+    }
+
+    @AfterEach
+    public void endTime() {
+        System.out.println("Test finished at: " + Instant.now());
+    }
+
+    @Test
+    public void testAdd() {
+        // Given
+        double a = 2.5;
+        double b = 3.5;
+
+        // When
+        double result = Calculator.add(a, b);
+
+        // Then
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void testSub() {
+        // Given
+        double a = 7.25;
+        double b = 2.25;
+
+        // When
+        double result = Calculator.sub(a, b);
+
+        // Then
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testMul() {
+        // Given
+        double a = 2.5;
+        double b = 3;
+
+        // When
+        double result = Calculator.mul(a, b);
+
+        // Then
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testDiv() {
+        // Given
+        double a = 5;
+        double b = 3;
+
+        // When
+        double result = Calculator.div(a, b);
+
+        // Then
+        assertEquals(2.5, result);
+    }
+
+    @Test
+    public void testDivisionByZero() {
+        // Given
+        double a = 5;
+        double b = 0;
+
+        // When & Then
+        assertThrows(ArithmeticException.class, () -> Calculator.div(a, b));
+    }
+}
